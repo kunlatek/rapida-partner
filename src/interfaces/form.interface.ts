7aa435d022bf705eb761db806a503e25fa71e16d
ryzzansalman <ryzzan.salman@kunlatek.com>
2025-10-02 10:47:02 -0300

    Adiciona a propriedade isHidden à interface IContractRequestField e ao esquema de referência

--- src/interfaces/form.interface.ts
+++ src/interfaces/form.interface.ts
@@ -64,6 +64,7 @@ export interface IContractRequestField {
   dataType: EDataType;
   requiredOneOf?: string[];
   isRequired?: boolean;
+  isHidden?: boolean;
   foreignKey?: IContractRequestFieldForeignKey;
   isPrimaryKey?: boolean;
   minSize?: number;
