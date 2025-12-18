c9bd3943434316b29c9cc4048bf4d10f9f6e9b8a
Ryzzan Abbade Salman <ryzzan.salman@kunlatek.com>
2025-12-18 14:37:17 -0300

    :recycle: switch project example from `administrado` to `movieBackoffice`

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import { tsProjectToJsonProject } from './src/controllers/ts_project_to_json_project';
-import { administrado } from '/home/ryzzan/Projects/Kunlatek/Rapida/rapida-projects/src/projects/administrado/administrado.ts';
+import { movieBackoffice } from './src/examples/projects/movieBackoffice';
 import { validateProjectJson } from './src/utils/json';
 
 /**
  * ts to json
  */
-const tsToJson = tsProjectToJsonProject(administrado);
+const tsToJson = tsProjectToJsonProject(movieBackoffice);
 
 /**
  * validate project json schema 
